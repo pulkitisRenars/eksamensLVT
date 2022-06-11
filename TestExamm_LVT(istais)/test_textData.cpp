a4f2104370198243455274a28ed328e6f03db006
renars <renars.pulkis@gmail.com>
2022-06-11 09:48:27 +0300

    Testa pilveidošana, bet nav pilnveidoti jautajumi un izskats

--- TestExamm_LVT(istais)/test_textData.cpp
+++ TestExamm_LVT(istais)/test_textData.cpp
@@ -9,6 +9,22 @@ int nepareizi;
 
 string questions[10];
 
+ int asked[10];
+void randoms(int *arr, size_t n)
+{
+    if (n > 1) 
+    {
+        size_t i;
+        srand(time(NULL));
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = arr[j];
+          arr[j] = arr[i];
+          arr[i] = t;
+        }
+    }
+}
 
 
 void infoTest(){
@@ -16,28 +32,46 @@ cout<<"INFO";
 }
 
 void mainTest(){
-    srand (time(NULL));
-  int answers1[10] = {23,13,13,14,24,12,23,13,1,1};
-  int answers2[10] = {32,31,31,41,42,21,32,31,1,1};
-  int asked[10];
-   questions[0] = "Priekš kā izmanto cout funkciju?\n1 - Ievadīt tekstu konsolē | 2 - Izvadīt tekstu konsolē | 3 - Izvadīt mainīgo vērtību konsolē | 4 - Ievadīt mainīgo vērtību konsolē";
-   questions[1] = "Priekš kā izmanto cin funkciju?\n1 - Ievadīt tekstu konsolē | 2 - Izvadīt tekstu konsolē | 3 - Ievadīt mainīgo vērtību konsolē | 4 - izvadīt mainīgo vērtību konsolē";
-   questions[2] = "Ar kurām funkcijām var izvadīt tekstu konsolē?\n1 - cout | 2 - System.out.print() | 3 - printf() | 4 - print()";
-   questions[3] = "Ar kurām funkcijām var ievadīt tekstu konsolē?\n1 - cin | 2 - Scanner | 3 - input()| 4 - izvadot iepriekš mainīgā saglabāto vērtību";
-   questions[4] = "Kam ir domāts ofstream?\n1 - Faila lasīšanai | 2 - Faila atvēršanai programmai | 3 - Faila izdzēšanai | 4 - Faila rakstīšanai";
-   questions[5] = "Kam ir domāts ifstream?\n1 - Faila atvēršanai programmai | 2 - Faila lasīšanai | 3 - Faila rakstīšanai | 4 - Faila izdzēšanai";
-   questions[6] = "Ko dara ios::app?\n1 - Izdzēš faila saturu | 2 - Neietekmē faila saturu | 3 - Atļauj turpināt faila rakstīšanu bez satura dzēšanas | 4 - Atļauj turpināt faila rakstīšanu ar satura dzēšanu";
-   questions[7] = "Kas no piemēriem ir patiesība par string?\n1 - string vertība ir char masīvs | 2 - string vertība ir string masīvs | 3 - Divas string vērtības nevar sasummēt kopā | 4 - Divas string vērtības var sasummēt kopā";
-   questions[8] = "b;ank";
-   questions[9] = "blank";
+
+    int s = 1;
+  int answers1[10] = {23,13,13,14,24,12,23,13,23,1};
+  int answers2[10] = {32,31,31,41,42,21,32,31,32,1};
+ int answer;
+   questions[0] = "\nPriekš kā izmanto cout funkciju?\n1 - Ievadīt tekstu konsolē |\n2 - Izvadīt tekstu konsolē |\n3 - Izvadīt mainīgo vērtību konsolē |\n4 - Ievadīt mainīgo vērtību konsolē|";
+   questions[1] = "\nPriekš kā izmanto cin funkciju?\n1 - Ievadīt tekstu konsolē |\n2 - Izvadīt tekstu konsolē |\n3 - Ievadīt mainīgo vērtību konsolē |\n4 - izvadīt mainīgo vērtību konsolē|";
+   questions[2] = "\nAr kurām funkcijām var izvadīt tekstu konsolē?\n1 - cout |\n2 - System.out.print() |\n3 - printf() |\n4 - print()|";
+   questions[3] = "\nAr kurām funkcijām var ievadīt tekstu konsolē?\n1 - cin |\n2 - Scanner |\n3 - input()|\n4 - izvadot iepriekš mainīgā saglabāto vērtību|";
+   questions[4] = "\nKam ir domāts ofstream?\n1 - Faila lasīšanai |\n2 - Faila atvēršanai programmai |\n3 - Faila izdzēšanai |\n4 - Faila rakstīšanai|";
+   questions[5] = "\nKam ir domāts ifstream?\n1 - Faila atvēršanai programmai |\n2 - Faila lasīšanai |\n3 - Faila rakstīšanai |\n4 - Faila izdzēšanai|";
+   questions[6] = "\nKo dara ios::app?\n1 - Izdzēš faila saturu |\n2 - Neietekmē faila saturu |\n3 - Atļauj turpināt faila rakstīšanu bez satura dzēšanas |\n4 - Atļauj turpināt faila rakstīšanu ar satura dzēšanu|";
+   questions[7] = "\nKas no piemēriem ir patiesība par string?\n1 - string vertība ir char masīvs |\n2 - string vertība ir string masīvs |\n3 - Divas string vērtības nevar sasummēt kopā |\n4 - Divas string vērtības var sasummēt kopā|";
+   questions[8] = "\nKas no piemēriem ir patiesība par char?\n1 - char vertība ir vairāku simbolu virkne |\n2 - char vērtību var sasummēt ar string vērtību |\n3 - char vērtība ir jāraksta \" '' \" |\n4 - char vērtība ir jāraksta \" \"\" \"|";
+   questions[9] = "\n";
 pareizi = 0; nepareizi=0;
 
+ 
+    int asked[10];
+    for (int i=0; i<10; i++){
+        asked[i] = i;
+    }
+    randoms(asked, 10);
+
 for(int i = 0;i<10;i++){
-cout<<i++<<". jautājums";
-cout<<rand() % 10;
-
+   cout<<"\n----------------------------------------------------------------------------------";
+cout<<"\n\n"<<s++<<". jautājums";
+cout<<questions[asked[i]];
+cout<<"\n\nTava atbilde: ";cin>>answer;
+if(answer==answers1[asked[i]]){
+  pareizi++;
+  cout<<"Pareizi";
+}else if (answer == answers2[asked[i]]){
+  pareizi++;
+  cout<<"Pareizi";
+}else if (answer!= answers1[asked[i]] && answer != answers2[asked[i]]){
+  nepareizi++;
+  cout<<"Nepareizi";
 }
-
+}
 ofstream writeFile("rezultati.txt",ios::app);
 writeFile<<"Pareizie: "<<pareizi<<", Nepareizie: "<<nepareizi<<"\n";
 writeFile.close();
